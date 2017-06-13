79c36ddb2608f263da13af79882a695c53c32ee2
fingertippower <939408507@qq.com>
2017-06-13 13:38:50 +0800

    mywebsite

---
+++ application/views/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <base href="<?php echo site_url()?>">
+    <title>Document</title>
+</head>
+<body>
+<div id="app"></div>
+<script src="./vue/js/index.js"></script>
+</body>
+</html>
---
+++ vue/components/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <p>999988888</p>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+
+</style>
