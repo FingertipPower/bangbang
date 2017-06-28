fa476c2256675157d4637cca8ed32603ce81f33c
fingertippower <939408507@qq.com>
2017-06-28 22:22:20 +0800

    简历页面的构建

--- vue/components/resume.vue
+++ vue/components/resume.vue
@@ -9,6 +9,12 @@
                     拥有良好的自学习惯，对于编程有无限的探知能力。能够有效的与团队成员进行沟通，增强团队协作的效率。对于项目中所遇到的问题能够
                     思考到最优的解决方案。
                 </p>
+                <div class="project">
+                    <div class="project-title">
+                        <div class="project-title-imgbox"><img src="../src/assets/img/1.png" class="project-img"></div>
+                        <span>项目经历</span>
+                    </div>
+                </div>
             </div>
             <div class="resume-right">
 
@@ -27,23 +33,23 @@
 
 <style lang="scss" scoped>
     #resume{
-        width: 90%;
+        width: 80%;
         height: 500px;
         margin-top: 60px;
-        margin-left: 5%;
+        margin-left: 10%;
     }
     .resume-left{
-        background-color: #fafafa;
         width: 60%;
         height: 500px;
         float: left;
         padding-left: 4%;
         padding-top: 3%;
         padding-right: 4%;
+        background-color: #fafafa;
     }
     .resume-right{
         background-color: #ececec;
-        width: 32%;
+        width: 25%;
         height: 500px;
         float: left;
     }
@@ -73,6 +79,41 @@
         }
         .resume-left-article{
             font-size: .3em;
+        }
+    }
+    .project{
+        margin-top: 3%;
+        height: 200px;
+        width: 100%;
+    }
+    .project-img{
+        width: 100%;
+        height: 100%;
+        margin: 0 auto;
+        display: block;
+        float: left;
+    }
+    .project-title{
+        width: 100%;
+        height: 40px;
+        line-height: 40px;
+    }
+    .project-title-imgbox{
+        width: 6%;
+        height: 80.3%;
+        background-color: rgba(197, 51, 100, 1);
+        border-radius: 20px;
+        float: left;
+    }
+    @media screen and (max-width: 600px){
+        .project-title-imgbox{
+            width: 15%;
+            height: 15%;
+        }
+    }
+    @media screen and (max-width: 1200px){
+        .project-title-imgbox{
+            width: 8.3%;
         }
     }
 </style>
