bab9e0503643196d09e712a13b0982d98f5d9c91
fingertippower <939408507@qq.com>
2017-07-19 18:18:52 +0800

    ci部分的测试

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -3,6 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Index extends CI_Controller {
 	public function main(){
-        $this->load->view('index');
+//        $this->load->view('index');
+        $this->load->model('cheshi');
+        $bbb = $this->cheshi->aaa();
+        echo $bbb;
 	}
 }
--- application/views/index.php
+++ application/views/index.php
@@ -6,7 +6,8 @@
     <title>Document</title>
 </head>
 <body>
-<div id="app"></div>
-<script src="./vue/js/index.js"></script>
+<!--<div id="app"></div>-->
+<!--<script src="./vue/js/index.js"></script>-->
+<p>qqqqqqqq</p>
 </body>
 </html>
